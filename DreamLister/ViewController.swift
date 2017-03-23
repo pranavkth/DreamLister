376ddeaa7014e70f0049e67f344a00ed96ba92b6
pranavkth <pranavkth@gmail.com>
2017-03-23 21:56:07 +0530

    Delete button removed

--- DreamLister/ViewController.swift
+++ DreamLister/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         tableview.delegate = self
         //generateTestData()
         attemptFetch()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     
